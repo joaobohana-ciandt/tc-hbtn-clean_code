f750771c8e8cada42a13d650dbf1363a6a620c26
Joao Bohana <jbohana@ciandt.com>
2022-07-18 11:59:57 -0300

    task01 fix

--- 0x00/function/Functions.java
+++ 0x00/function/Functions.java
@@ -16,12 +16,12 @@ public class Functions{
    //A
     public void saveUser(User user){};
 
-   //B
+   //A
     public User saveUser(User user){
-        return userRepository.save(user);
+      if(user.isAdmin){
+      user.setRole(user);
     }
-    public void setRole(User user){
-        user.setRole(user);
+      return userRepository.save(user);
     }
 
    //B
